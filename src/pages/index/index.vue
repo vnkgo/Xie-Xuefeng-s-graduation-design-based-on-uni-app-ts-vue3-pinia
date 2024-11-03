01f4f08d72744c5348f5d41e320224052f5efd0a
vnkgo <1272499410@qq.com>
2024-11-03 21:15:51 +0800

    feat:轮播图类型定义和渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
 import XtzSwiper from '@/components/XtzSwiper.vue'
 import CustomNavbar from './components/CustomNavbar.vue'
+import { getHomeBannerApi } from '@/services/home'
+import { onLoad } from '@dcloudio/uni-app'
+import type { BannerItem } from '@/types/home'
+import { ref } from 'vue'
+
+const bannerList = ref<BannerItem[]>([])
+
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerApi()
+  console.log(res)
+  bannerList.value = res.result
+}
+
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
   <CustomNavbar />
-  <XtzSwiper />
+  <XtzSwiper :list="bannerList" />
   <view class="index">index</view>
 </template>
 
---
+++ src/services/home.ts
@@ -0,0 +1,10 @@
+import type { BannerItem } from '@/types/home'
+import { http } from '@/utils/http'
+
+export const getHomeBannerApi = (distributionSite = 1) => {
+  return http<BannerItem[]>({
+    url: '/home/banner',
+    method: 'GET',
+    data: { distributionSite },
+  })
+}
